8dc975c018c2c9f23cfcd376f0450a8ec0a33c6b
Eduarda França <mariaeduardafrancasantos@gmail.com>
2021-08-17 21:59:00 -0300

    Aula 0409

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const vm = new Vue({
   el: "#app",
   data: {
       produtos: [],
-      produto: false
+      produto: false,
+      carrinho: [],
   },
   filters: {
     numeroPreco(valor) {
       return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
+    }
+  },
+  computed: {
+    // Função que calcula o valor total do carrinho
+    carrinhoTotal() {
+      let total = 0;
+      // Verifica se existe item no carrinho
+      if(this.carrinho.length) {
+        // item é nome, id e preço de cada produto no carrinho
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        });
+      }
+      return total;
     }
   },
   methods: {
@@ -23,6 +38,29 @@ const vm = new Vue({
         .then(r => {
           this.produto = r;
         })
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    fecharModal({target, currentTarget}) {
+      if(target === currentTarget) {
+        this.produto = false
+      }
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      // Disistruturando o objeto produto: coloca const { nome das variaveis que serão utilizadas } = this.objeto
+      const { id, nome, preco } = this.produto;
+      // Adicionando no array carrinho
+      this.carrinho.push({ id, nome, preco });
+    },
+    removerItem(index) {
+      // Remove 1 item do carrinho.
+      this.carrinho.splice(index, 1);
     }
   },
   created() {
